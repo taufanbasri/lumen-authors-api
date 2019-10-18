c93a8e6417039e56ca278025c6332bfefb659e7e
taufanprasetyobasri <taufanprasetyobasri@gmail.com>
2019-10-18 11:20:50 +0700

    Basic structure for Authors

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        factory(App\Author::class, 50)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('authors', 'AuthorController@index');
+$router->post('authors', 'AuthorController@store');
+$router->get('authors/{author}', 'AuthorController@show');
+$router->get('authors', 'AuthorController@index');
+$router->put('authors/{author}', 'AuthorController@update');
+$router->patch('authors/{author}', 'AuthorController@update');
+$router->delete('authors/{author}', 'AuthorController@destroy');
